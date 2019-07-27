d63daeb6d26c7cde35f65391cd98e75246f2b8aa
BlackJibert <1257699625@qq.com>
2019-07-27 14:08:39 +0800

    增加Flash信息

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for,flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -31,7 +31,10 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        session['name'] = form.name.data
+        old_name=session.get('name')
+        if old_name is not None adn old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+            session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
 
