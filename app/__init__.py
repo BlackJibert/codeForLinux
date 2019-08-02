3a1d91625d7af578956abec5d614b4bed7e8b06d
BlackJibert <1257699625@qq.com>
2019-08-02 19:22:44 +0800

    大型项目结构

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import flask
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+bootstrap = Bootstrap()
+mail = Mail()
+momont=Moment()
+db=SQLAlchemy()
+
+def create_app(config_name):
+	app=Flask(__name__)
+	app.config.from_object(config[config_name])
+	config[config_name].init_app(app)
+	bootstrap.init_app(app)
+	mail.init_app(app)
+	momont.init_app(app)
+	db.init_app(app)
+
+	from .main import main as main_blueprint
+	app.register_blueprint(main_blueprint)
+	return app
